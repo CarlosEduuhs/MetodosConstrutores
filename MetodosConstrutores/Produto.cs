09dd31911895230ba333c8cd1a2c968c205f7961
CarlosEduuhs <sobralcarlos11@gmail.com>
2021-09-21 22:13:33 -0300

    Adicionar arquivos de projeto.

---
+++ MetodosConstrutores/Produto.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetodosConstrutores
+{
+    public class Produto
+    {
+        //Atributos
+        private string _nome;
+        private double _preco;
+        //AutoProperty - prop tab 2x
+        public double Estoque { get; private set; }
+        
+
+        //Metodo construtor - Construtor Padrao
+        public Produto()
+        {
+        }
+
+        public Produto(string valNome, double valPreco, double valEstoque)
+        {
+            _nome = valNome;
+            _preco = valPreco;
+            Estoque = valEstoque;
+        }
+
+        public Produto(string valNome)
+        {
+            _nome = valNome;
+        }
+
+        //Encapsulamento - GET e SET
+        public string GetNome()
+        {
+            return _nome;
+        }
+
+        public string SetNome(string nome)
+        {
+            if (nome.Length < 5)
+            {
+                return "Nome Não Alterado, valor com menos de 5 caracteres...";
+            }
+            else
+            {
+                _nome = nome;
+                return "Nome Alterado com sucesso!";
+            }
+           
+        }
+
+        // Encapsulamento por propriedade
+        public double Preco
+        {
+            get { return _preco; }
+            set { _preco = value; }
+        }
+
+        public void SomaEstoque(double estoque)
+        {
+            Estoque += estoque; 
+        }
+
+        //Metodos - Void ou Retorno || Com ou Sem parametro
+        public double ValorEstoque()
+        {
+            return _preco * Estoque;
+        }
+
+    }
+}
---
+++ MetodosConstrutores/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetodosConstrutores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Criacao de Classe - Atributos e Métodos
+            //Produto prod1 = new Produto();
+            //prod1.SetNome("Monitor");
+            //prod1.preco = 630.80;
+            //prod1.estoque = 80;
+            //Console.WriteLine(prod1.preco * prod1.estoque);
+            //Produto prod2 = new Produto();
+            //prod2.SetNome("Mouse");
+            //prod2.preco = 28.99;
+            //prod2.estoque = 150;
+            //Console.WriteLine(prod2.ValorEstoque().ToString("C"));
+
+            //Produto prod3 = new Produto("Teclado", 21.35, 200);
+            //Console.WriteLine(prod3.ValorEstoque().ToString("C"));
+
+            //Produto prod4 = new Produto("Cabo HDMI");
+            //prod4.preco = 7.50;
+            //prod4.estoque = 50;
+            //Console.WriteLine(prod4.ValorEstoque().ToString("C"));
+
+
+            //Metodos Construtores; sobreposição de metodos; encapsulamento
+
+
+
+            Produto produto = new Produto("Monitor", 650.50, 50);
+            Console.WriteLine(produto.GetNome());
+            Console.WriteLine(produto.SetNome("Monitor 21.5"));
+            produto.Preco = 550.90;
+            produto.SomaEstoque(200);
+            Console.WriteLine(produto.Estoque);
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
